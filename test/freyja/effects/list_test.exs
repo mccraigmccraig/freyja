55c824e990109c5c24b711222c1b4a22bd0d8a23
mccraigmccraig <mccraigmccraig@gmail.com>
2025-10-30 19:47:46 +0000

    Coroutine + List + State test

--- test/freyja/effects/list_test.exs
+++ test/freyja/effects/list_test.exs
@@ -7,6 +7,7 @@ defmodule Freyja.Effects.ListTest do
 
   alias Freyja.Effects.Coroutine
   alias Freyja.Effects.List
+  alias Freyja.Effects.State
   alias Freyja.Run
 
   defcon yield_to_pair(a), [Coroutine] do
@@ -18,18 +19,6 @@ defmodule Freyja.Effects.ListTest do
     # fx_map is like Enum.map, but allows effects
     # including Coroutine.yield in the mapper function
     r <- fx_map(l, &yield_to_pair/1)
-    return(r)
-  end
-
-  defcon yield_sum(element, acc), [Coroutine] do
-    r <- yield({element, acc})
-    return(acc + r)
-  end
-
-  defcon sum_with_yield(l), [List] do
-    # fx_reduce is like Enum.reduce, but allows effects
-    # including Coroutine.yield in the reducer function
-    r <- fx_reduce(l, 0, &yield_sum/2)
     return(r)
   end
 
@@ -71,6 +60,18 @@ defmodule Freyja.Effects.ListTest do
     end
   end
 
+  defcon yield_sum(element, acc), [Coroutine] do
+    r <- yield({element, acc})
+    return(acc + r)
+  end
+
+  defcon sum_with_yield(l), [List] do
+    # fx_reduce is like Enum.reduce, but allows effects
+    # including Coroutine.yield in the reducer function
+    r <- fx_reduce(l, 0, &yield_sum/2)
+    return(r)
+  end
+
   describe "reduce" do
     test "it reduces a list" do
       runner =
@@ -101,6 +102,60 @@ defmodule Freyja.Effects.ListTest do
 
       Logger.error(
         "#{__MODULE__}.reduce_outcome\n" <> inspect(final_outcome.outputs.logger, pretty: true)
+      )
+    end
+  end
+
+  defcon multiply_and_sum(element), [Coroutine, State] do
+    y <- yield(element)
+    product <- return(element * y)
+    current_sum <- get()
+    put(current_sum + product)
+    return(product)
+  end
+
+  defcon process_list_with_state(l), [List] do
+    products <- fx_map(l, &multiply_and_sum/1)
+    return(products)
+  end
+
+  describe "map with state" do
+    test "it maps with coroutine and maintains state sum" do
+      runner =
+        Run.with_handlers(
+          logger: Freyja.Effects.EffectLogger.Handler,
+          l: List.Handler,
+          c: Coroutine.Handler,
+          s: {State.Handler, 0}
+        )
+
+      input = [10, 20, 30]
+      # Yield responses - multiply each element by these
+      responses = [2, 3, 4]
+
+      computation = process_list_with_state(input)
+
+      # feed the coroutine yields with responses
+      final_outcome =
+        Enum.reduce(
+          responses,
+          Run.run(computation, runner),
+          fn response, outcome ->
+            Run.resume(outcome, response)
+          end
+        )
+
+      # Products: [10*2, 20*3, 30*4] = [20, 60, 120]
+      assert final_outcome.result == %Freyja.OkResult{
+               value: [20, 60, 120]
+             }
+
+      # Sum: 20 + 60 + 120 = 200
+      assert final_outcome.outputs.s == 200
+
+      Logger.error(
+        "#{__MODULE__}.map_with_state_outcome\n" <>
+          inspect(final_outcome.outputs.logger, pretty: true)
       )
     end
   end

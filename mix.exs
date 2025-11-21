2108d7e9501ea8e6ae7d5ee552cb3282cc4492d8
mccraigmccraig <mccraigmccraig@gmail.com>
2025-11-21 17:32:49 +0000

    update mix.deps with package info

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule Freyja.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/mccraigmccraig/freyja",
+      homepage_url: "https://github.com/mccraigmccraig/freyja",
+      docs: [
+        main: "readme",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -21,13 +29,31 @@ defmodule Freyja.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:mix_test_watch, "~> 1.4.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
       {:uuid, "~> 1.1"},
       {:jason, "~> 1.4"}
+    ]
+  end
+
+  defp description do
+    """
+    Algebraic effects and handlers for Elixir using the Hefty Algebras architecture.
+    Enables writing programs as pure functions with effects as data structures,
+    providing clean separation between effect description and interpretation.
+    """
+  end
+
+  defp package do
+    [
+      name: "freyja",
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE),
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/mccraigmccraig/freyja"
+      }
     ]
   end
 end
